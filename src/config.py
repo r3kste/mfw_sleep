7db3c56cd6a37b2927c203c84361d851f98f8cee
r3kste <138380708+r3kste@users.noreply.github.com>
2025-04-24 19:01:56 +0530

    Refactor configuration paths to use dynamic project directory resolution for trained models and recorded frames

--- src/config.py
+++ src/config.py
@@ -1,10 +1,17 @@
+import os
+
 IP = "192.168.0.103"
 PORT = 6969
 HEADER_SIZE = 4
 BUFFER_SIZE = 1024
 
-TRAINED_MODELS_DIR = "/home/r3kste/code/mfw_sleep/trained"
-RECORDED_FRAMES_DIR = "/home/r3kste/code/mfw_sleep/output"
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# /mfw_sleep/trained
+TRAINED_MODELS_DIR = os.path.join(PROJECT_DIR, "trained")  
+
+# /mfw_sleep/output
+RECORDED_FRAMES_DIR = os.path.join(PROJECT_DIR, "output")
 
 subfolders = {
     "open": 1,
