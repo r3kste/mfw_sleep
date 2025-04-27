dd4cddf8a8bfc6f46503f7ee88cfd7b0dacc357e
Schöpfer <portandcollect@gmail.com>
2025-04-28 00:08:30 +0530

    Added folder to add mount and alive code for debug

---
+++ src/webserver/GUY_ALIVE.py
@@ -0,0 +1,18 @@
+import socket
+
+# Sending a warning manually for testing purposes.
+HOST = '255.255.255.255'  # Example: localhost
+PORT =    5005     # Example: port number
+
+def send_message():
+    message = "GUY_ALIVE"
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        try:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Enable broadcasting
+            s.sendto(message.encode('utf-8'), (HOST, PORT))
+            print(f"Message '{message}' sent to {HOST}:{PORT} via UDP")
+        except Exception as e:
+            print(f"Failed to send message: {e}")
+
+if __name__ == "__main__":
+    send_message()
